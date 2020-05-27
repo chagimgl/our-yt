6c493e32de92e97338b222fb8ea799ac8d5b9a24
Chandmani-Erden Tsend-Ayush <tsendayush@email.arizona.edu>
2020-05-26 20:57:21 -0700

    initial xcode project creation

---
+++ our-yt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  our-yt
+//
+//  Created by Chandmani-Erdene Tsend-Ayush on 5/26/20.
+//  Copyright © 2020 Chandmani-Erdene Tsend-Ayush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
